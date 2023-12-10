910798ef5aa76836f06bbbc77d1830d5abf3d447
DESKTOP-D2888SQ\Admin <lactuannn@gmail.com>
2023-12-10 19:59:35 +0700

    czxczx

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,6 +4,7 @@ import shopify from './shopify.png'
 import starbucks from './starbucks.png'
 import tesla from './tesla.png'
 import logo from './logo.svg'
+import fsoft from './fsoft.svg'
 
 export {
     hero,
@@ -11,5 +12,6 @@ export {
     shopify,
     starbucks,
     tesla,
-    logo
+    logo,
+    fsoft
 }
